e9a9a733400f1c8e2e4c2daf4a67438aa705d3d0
LNWatta <helene.finot@le-campus-numerique.fr>
2024-01-16 12:09:31 +0100

    update header

---
+++ header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>CV Helene Finot
+    </title>
+</head>
